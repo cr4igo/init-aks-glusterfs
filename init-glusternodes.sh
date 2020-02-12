a3efeb3e240a35c4c7cc88101246c8071bd03e71
Markus Priegl <markus.priegl@gmail.com>
2020-02-12 01:40:11 +0100

    Update and rename init.sh to init-glusternodes.sh

--- init-glusternodes.sh
+++ init-glusternodes.sh
@@ -8,11 +8,12 @@
 modprobe dm_snapshot
 modprobe dm_mirror
 modprobe dm_thin_pool
+modprobe fuse
 
 apt-get update -y
 apt-get install software-properties-common -y
 add-apt-repository ppa:gluster/glusterfs-7
 apt-get update -y
-apt-get install -y glusterfs-fuse
+apt-get install -y glusterfs-fuse attr
 
 df -h
