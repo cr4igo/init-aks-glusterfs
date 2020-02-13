c6924332a61a7986942b47cb045281e3b1ab438c
Markus Priegl <markus.priegl@gmail.com>
2020-02-13 18:15:01 +0100

    Update init-glusternodes.sh

--- init-glusternodes.sh
+++ init-glusternodes.sh
@@ -2,6 +2,7 @@ modprobe dm_snapshot
 modprobe dm_mirror
 modprobe dm_thin_pool
 modprobe fuse
+apt-get install software-properties-common -y
 add-apt-repository ppa:gluster/glusterfs-7
 apt-get update -y
-apt-get install -y software-properties-common glusterfs-fuse glusterfs-client attr openssh-server vim wget
+apt-get install -y glusterfs-client glusterfs-fuse attr openssh-server vim wget
