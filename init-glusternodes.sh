b33f7441faf43317265eea3dd84855838abf63e1
Markus Priegl <markus.priegl@gmail.com>
2020-02-13 18:50:56 +0100

    Update init-glusternodes.sh

--- init-glusternodes.sh
+++ init-glusternodes.sh
@@ -1,3 +1,10 @@
+#iptables -N HEKETI
+#iptables -A HEKETI -p tcp -m state --state NEW -m tcp --dport 24007 -j ACCEPT
+#iptables -A HEKETI -p tcp -m state --state NEW -m tcp --dport 24008 -j ACCEPT
+#iptables -A HEKETI -p tcp -m state --state NEW -m tcp --dport 2222 -j ACCEPT
+#iptables -A HEKETI -p tcp -m state --state NEW -m multiport --dports 49152:49251 -j ACCEPT
+#service iptables save
+
 modprobe dm_snapshot
 modprobe dm_mirror
 modprobe dm_thin_pool
@@ -7,4 +14,7 @@ apt-get update -y
 apt-get install software-properties-common -y
 add-apt-repository ppa:gluster/glusterfs-7
 apt-get update -y
-apt-get install -y glusterfs-client glusterfs-fuse attr openssh-server vim wget
+apt-get install -y glusterfs-fuse attr openssh-server vim wget
+apt-get install -y glusterfs-client
+
+df -h
