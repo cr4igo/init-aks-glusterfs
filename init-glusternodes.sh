7eb115bedcdbd4f9018b15690f69eb6fd590e91e
Markus Priegl <markus.priegl@gmail.com>
2020-02-13 18:56:06 +0100

    Update init-glusternodes.sh

--- init-glusternodes.sh
+++ init-glusternodes.sh
@@ -1,9 +1,4 @@
-#iptables -N HEKETI
-#iptables -A HEKETI -p tcp -m state --state NEW -m tcp --dport 24007 -j ACCEPT
-#iptables -A HEKETI -p tcp -m state --state NEW -m tcp --dport 24008 -j ACCEPT
-#iptables -A HEKETI -p tcp -m state --state NEW -m tcp --dport 2222 -j ACCEPT
-#iptables -A HEKETI -p tcp -m state --state NEW -m multiport --dports 49152:49251 -j ACCEPT
-#service iptables save
+#!/bin/bash
 
 modprobe dm_snapshot
 modprobe dm_mirror
